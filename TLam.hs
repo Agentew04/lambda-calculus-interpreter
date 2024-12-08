58e87c6850d3c474220a5d942fbc80eec06d82e6
Rodrigo Appelt <rappelt@inf.ufsm.br>
2024-12-08 14:49:06 -0300

    add coisas da ultima aula

--- TLam.hs
+++ TLam.hs
@@ -17,13 +17,20 @@ freeVars (App t1 t2) = freeVars t1 ++ freeVars t2
 -- [x |-> s] (t1 t2) => ([x |-> s] t1) ([x |-> s] t2)
 subs :: Char -> Term -> Term -> Term
 subs x s (Var z) = if x == z then s else Var z
-subs x s (Abs y t) = Abs y (subs x s t)
+subs x s (Abs y t1) = if x==y then Abs y t1 else Abs y (subs x s t1)
 subs x s (App t1 t2) = App (subs x s t1) (subs x s t2) 
 
--- ainda falta transformar em call by value
--- agora eh um full beta reduction
+isVal :: Term -> Bool
+isVal (Var x) = True
+isVal (Abs x t) = True
+isVal _ = False
+
+-- transformada em call by value
+-- soh aplica subst quando 
 eval :: Term -> Term
 eval (Var x) = Var x
-eval (Abs x t) = Abs x (eval t)
-eval (App (Abs x t1) t2) = eval (subs x t2 t1)
-eval (App t1 t2) = App (eval t1) (eval t2)
+eval (Abs x t) = Abs x t
+eval (App (Abs x t1) t2) = if isVal t2
+    then subs x t2 t1 -- E-APP-ABS
+    else let t2' = eval t2 in App t1 t2' -- E-APP-2
+eval (App t1 t2) = let t1' = eval t1 in App t1' t2
