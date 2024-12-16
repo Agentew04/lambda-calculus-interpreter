6ed74bcee0e8c321550d57941831234a64d6e20e
Pedro Ramos <pedrowarkenramos@gmail.com>
2024-12-16 13:27:32 -0300

    remove unused subs and eval functions from TLam.hs🔧

--- TLam.hs
+++ TLam.hs
@@ -15,23 +15,10 @@ freeVars (Var x)     = [x]
 freeVars (Abs x t)   = delete x (freeVars t)
 freeVars (App t1 t2) = freeVars t1 ++ freeVars t2
 
-subs :: Char -> Term -> Term -> Term
-subs x s (Var z) = if x == z then s else Var z
-subs x s (Abs y t1) = if x==y then Abs y t1 else Abs y (subs x s t1)
-subs x s (App t1 t2) = App (subs x s t1) (subs x s t2) 
-
 isVal :: Term -> Bool
 isVal (Var x) = True
 isVal (Abs x t) = True
 isVal _ = False
-
-eval :: Term -> Term
-eval (Var x) = Var x
-eval (Abs x t) = Abs x t
-eval (App (Abs x t1) t2) = if isVal t2
-    then subs x t2 t1
-    else let t2' = eval t2 in App t1 t2'
-eval (App t1 t2) = let t1' = eval t1 in App t1' t2
 
 shifting :: TermNL -> Int -> Int -> TermNL
 shifting (VarNL k) d c = if k<c
